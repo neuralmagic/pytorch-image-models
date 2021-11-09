d6dbe6343fbe1be697ddd3bd6afeb63032f98823
Konstantin Gulin <konstantin@neuralmagic.com>
2021-11-09 09:42:56 -0800

    Add WandB Logger

--- train.py
+++ train.py
@@ -629,7 +629,7 @@ def main():
 
     # main SparseML integration
     sparseml_loggers = (
-        [PythonLogger(), TensorBoardLogger(log_path=output_dir)]
+        [PythonLogger(), TensorBoardLogger(log_path=output_dir), WANDBLogger()]
         if output_dir
         else None
     )
